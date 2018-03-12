2b99f9f39a662c0ec78cf491fb7dcffb7f4edcd1
Geoff Hing <ghing@apmreports.org>
2018-03-12 10:11:54 -0500

    Mitigate Python path issues
    On my work Mac, I have multiple Python versions installed, including the
    Mac system version, the one installed by Homebrew, which is also the one
    that vim uses, and ones installed with pyenv. Make it possible for the
    plugin to find a version of dailynotes relative to itself.

--- dailynotes.vim/plugin/dailynotes.py
+++ dailynotes.vim/plugin/dailynotes.py
@@ -1,6 +1,23 @@
 import vim
 
-from dailynotes.util import get_note_path
+try:
+    from dailynotes.util import get_note_path
+
+except ModuleNotFoundError:
+    # Couldn't find dailynotes.util.
+    # This is likely because of issues with multiple versions of Python.
+    # If it's not already on our path, point it up a few directories, which is
+    # where the Python package will also live.
+    import os.path
+    import sys
+
+    path = os.path.abspath(os.path.join(
+        os.path.dirname(vim.eval('expand(\"<sfile>\")')),
+        '..',
+        '..'
+    ))
+    sys.path.append(path)
+    from dailynotes.util import get_note_path
 
 
 def open_note():
